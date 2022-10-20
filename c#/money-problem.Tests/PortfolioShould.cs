bdb2b61b6be09c366b9a1ea79b57d9e0bf6aaae0
Cédric Hulin <hulin.cedric@gmail.com>
2022-10-20 16:16:27 +0200

    Add test3 & test4

--- c#/money-problem.Tests/PortfolioShould.cs
+++ c#/money-problem.Tests/PortfolioShould.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using money_problem.Domain;
@@ -38,6 +39,32 @@ public class PortfolioShould
         var usdEvaluation = portfolio.Evaluate(Currency.KRW, bank);
 
         usdEvaluation.Should().Be(2200);
+    }
+
+    [Fact(DisplayName = "5 USD + 10 EUR + 4 EUR = 21.8 USD")]
+    public void Test3()
+    {
+        var portfolio = new Portfolio();
+        portfolio.Add(5, Currency.USD);
+        portfolio.Add(10, Currency.EUR);
+        portfolio.Add(4, Currency.EUR);
+
+        var usdEvaluation = portfolio.Evaluate(Currency.USD, bank);
+
+        usdEvaluation.Should().Be(21.8);
+    }
+
+    [Fact(DisplayName = "1 EUR + 1 USD + 1 KRW = ? EUR")]
+    public void Test4()
+    {
+        var portfolio = new Portfolio();
+        portfolio.Add(1, Currency.EUR);
+        portfolio.Add(1, Currency.USD);
+        portfolio.Add(1, Currency.KRW);
+
+        var act = () => portfolio.Evaluate(Currency.EUR, bank);
+
+        act.Should().Throw<Exception>().WithMessage("TODO");
     }
 }
 
