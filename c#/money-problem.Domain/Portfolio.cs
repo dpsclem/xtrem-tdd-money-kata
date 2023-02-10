d20756d875ee7355671761541db4ac64622bc6d9
Cédric Hulin <hulin.cedric@gmail.com>
2023-02-10 15:58:06 +0100

    refator: Todo2 result type

--- c#/money-problem.Domain/Portfolio.cs
+++ c#/money-problem.Domain/Portfolio.cs
@@ -13,22 +13,51 @@ public class Portfolio
     {
         double total = 0;
         var missingExchangeRates = new List<MissingExchangeRateException>();
-        
-        moneys.ForEach(money =>
+
+        foreach (var money in moneys)
         {
-            try
-            {
-                total += bank.Convert(money, currency);
-            }
-            catch (MissingExchangeRateException missingExchangeRate)
-            {
-                missingExchangeRates.Add(missingExchangeRate);
-            }
-        });
-        
+            var result = Todo2(currency, bank, money);
+            if (result.HasResult)
+                total += result.Result.Value;
+            else
+                missingExchangeRates.Add(result.MissingExchangeRate);
+        }
+
         if (missingExchangeRates.Any())
             throw new MissingExchangeRatesException(missingExchangeRates);
 
         return total;
     }
+
+
+    private static Todo2Result Todo2(Currency currency, Bank bank, Money money)
+    {
+        try
+        {
+            return new Todo2Result(bank.Convert(money, currency));
+        }
+        catch (MissingExchangeRateException missingExchangeRate)
+        {
+            return new Todo2Result(missingExchangeRate);
+        }
+    }
+}
+
+internal class Todo2Result
+{
+    public Todo2Result(double convert)
+    {
+        Result = convert;
+        HasResult = true;
+    }
+
+    public Todo2Result(MissingExchangeRateException missingExchangeRate)
+    {
+        MissingExchangeRate = missingExchangeRate;
+        HasResult = false;
+    }
+
+    public bool HasResult { get; }
+    public MissingExchangeRateException? MissingExchangeRate { get; }
+    public double? Result { get; }
 }
